12bda310f87f3d22433a339604d72ddd53601137
Roman <roman199494@mail.ru>
2015-10-24 18:00:21 +0300

    class Parabola

---
+++ parabola.cpp
@@ -0,0 +1,53 @@
+#include "parabola.h"
+
+Parabola::Parabola(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3)
+{
+    points = new Matrix();
+    points = new Matrix();
+    points->addLine(x1, y1, z1, 0);
+    points->addLine(x2, y2, z2, 0);
+    points->addLine(x3, y3, z3, 0);
+    //x
+    Ax0 = points->getElement(0, 0);
+    Ax2 = 2.0*(points->getElement(2, 0) - 2.0*points->getElement(1, 0) + points->getElement(0, 0));
+    Ax1 = points->getElement(2, 0) - points->getElement(0, 0) - Ax2;
+    //y
+    Ay0 = points->getElement(0, 1);
+    Ay2 = 2.0*(points->getElement(2, 1) - 2.0*points->getElement(1, 1) + points->getElement(0, 1));
+    Ay1 = points->getElement(2, 1) - points->getElement(0, 1) - Ay2;
+    //z
+    Az0 = points->getElement(0, 2);
+    Az2 = 2.0*(points->getElement(2, 2) - 2.0*points->getElement(1, 2) + points->getElement(0, 2));
+    Az1 = points->getElement(2, 2) - points->getElement(0, 2) - Az2;
+}
+
+double Parabola::getX(double t)
+{
+    return Ax0 + Ax1*t + Ax2*t*t;
+}
+
+double Parabola::getY(double t)
+{
+    return Ay0 + Ay1*t + Ay2*t*t;
+}
+
+double Parabola::getZ(double t)
+{
+    return Az0 + Az1*t + Az2*t*t;
+}
+
+double *Parabola::getPoint(double t)
+{
+    double *point = new double[4];
+    point[0] = getX(t);
+    point[1] = getY(t);
+    point[2] = getZ(t);
+    point[3] = 0;
+    return point;
+}
+
+Parabola::~Parabola()
+{
+    //delete(points);
+}
+
---
+++ parabola.h
@@ -0,0 +1,21 @@
+#ifndef PARABOLA_H
+#define PARABOLA_H
+#include "matrix.h"
+
+class Parabola
+{
+private:
+    Matrix *points;
+    double Ax0, Ax1, Ax2;
+    double Ay0, Ay1, Ay2;
+    double Az0, Az1, Az2;
+public:
+    Parabola(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3);
+    double getX(double t);
+    double getY(double t);
+    double getZ(double t);
+    double *getPoint(double t);
+    ~Parabola();
+};
+
+#endif // PARABOLA_H
